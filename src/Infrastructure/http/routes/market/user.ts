94890c3f92d0f7f5e368e6fca2d328a5b239c02b
tugnodev <tugnodev@gmail.com>
2025-09-15 21:54:43 +0000

    init: merge developpement to main

--- src/Infrastructure/http/routes/market/user.ts
+++ src/Infrastructure/http/routes/market/user.ts
@@ -3,7 +3,6 @@ import { UserController } from '../../controllers/userController.js';
 import { UserUseCase } from '../../../../Application/usecases/userUseCase.js';
 import { UserRepoImpl } from '../../../repositories/userRepoImpl.js';
 
-// Initialize dependencies
 const userRepository = new UserRepoImpl();
 const userUseCase = new UserUseCase(userRepository);
 const userController = new UserController(userUseCase);
@@ -14,27 +13,19 @@ userRoutes.get('/', async (c) => {
     return c.json({ message: 'Hello User!' });
 });
 
-// Create a new user
 userRoutes.post('/register', async (c) => {
     return userController.createUser(c);
 });
-
-// Get user by ID
 userRoutes.get('/:id', async (c) => {
     return userController.getUserById(c);
 });
 
-// Update user
 userRoutes.patch('/update', async (c) => {
     return userController.updateUser(c);
 });
-
-// Delete user
 userRoutes.delete('/delete', async (c) => {
     return userController.deleteUser(c);
 });
-
-// Get all users (admin only)
 userRoutes.get('/all', async (c) => {
     return userController.getAllUsers(c);
 });
