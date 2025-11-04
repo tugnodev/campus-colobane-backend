951e6234a2eb718857ffecdd29f6fce16d8c5862
amadou2005 <diamankamadou9@gmail.com>
2025-11-04 21:43:13 +0000

    ControllerCategorie

--- src/Infrastructure/http/controllers/categorieController.ts
+++ src/Infrastructure/http/controllers/categorieController.ts
@@ -1,8 +1,47 @@
 import { CategorieUseCase } from "../../../Application/usecases/categorieUseCase.js";
 import type { categorieDto } from "../../../Application/dtos/categorie.js";
-//import type { Context } from "hono";
-//
-//export class CategorieController implements CategorieUseCase {
-//    
-//}
-//
+import type { Context } from "hono";
+
+export class CategorieController  {
+     private categorieUseCase: CategorieUseCase;
+     constructor(categorieUseCase: CategorieUseCase) {
+        this.categorieUseCase = categorieUseCase;
+     }
+
+    async create(ctx: Context) {
+            const orderData: categorieDto = await ctx.req.json();
+            const result = await this.categorieUseCase.create(orderData);
+            if (typeof result === "string") {
+                return ctx.json( "Error" );
+            }
+            return ctx.json(result);
+        }
+    
+        async update(ctx: Context) {
+            const orderData: categorieDto = await ctx.req.json();
+            const result = await this.categorieUseCase.update(orderData);
+            if (typeof result === "string") {
+                return ctx.json({ message: result });
+            }
+            return ctx.json(result);
+        }
+    
+        async delete(ctx: Context) {
+            const  name  = await ctx.req.json();
+            const result = await this.categorieUseCase.delete(name);
+            if (typeof result === "string") {
+                return ctx.json({ message: result });
+            }
+            return ctx.json({ message: result });
+        }
+    
+       async getAll(ctx: Context) {
+            const result = await this.categorieUseCase.getAll();
+            if (typeof result === "string") {
+                return ctx.json({ message: result });
+            }
+            return ctx.json({ message: result });
+        }
+       
+}
+
