aa090fd5c1d89a09449d37639ffc43c6c4a8cf33
ebeydi <ebeydif@gmail.com>
2025-11-08 14:31:38 +0000

    Sauvegarde avant merge

--- src/Infrastructure/http/controllers/categorieController.ts
+++ src/Infrastructure/http/controllers/categorieController.ts
@@ -1,57 +1,46 @@
 import { CategorieUseCase } from "../../../Application/usecases/categorieUseCase.js";
 import type { categorieDto } from "../../../Application/dtos/categorie.js";
-<<<<<<< HEAD
+
 import type { Context } from "hono";
 
-export class CategorieController  {
-     private categorieUseCase: CategorieUseCase;
-     constructor(categorieUseCase: CategorieUseCase) {
-        this.categorieUseCase = categorieUseCase;
-     }
+export class CategorieController {
+  private categorieUseCase: CategorieUseCase;
+  constructor(categorieUseCase: CategorieUseCase) {
+    this.categorieUseCase = categorieUseCase;
+  }
 
-    async create(ctx: Context) {
-            const orderData: categorieDto = await ctx.req.json();
-            const result = await this.categorieUseCase.create(orderData);
-            if (typeof result === "string") {
-                return ctx.json( "Error" );
-            }
-            return ctx.json(result);
-        }
-    
-        async update(ctx: Context) {
-            const orderData: categorieDto = await ctx.req.json();
-            const result = await this.categorieUseCase.update(orderData);
-            if (typeof result === "string") {
-                return ctx.json({ message: result });
-            }
-            return ctx.json(result);
-        }
-    
-        async delete(ctx: Context) {
-            const  name  = await ctx.req.json();
-            const result = await this.categorieUseCase.delete(name);
-            if (typeof result === "string") {
-                return ctx.json({ message: result });
-            }
-            return ctx.json({ message: result });
-        }
-    
-       async getAll(ctx: Context) {
-            const result = await this.categorieUseCase.getAll();
-            if (typeof result === "string") {
-                return ctx.json({ message: result });
-            }
-            return ctx.json({ message: result });
-        }
-       
+  async create(ctx: Context) {
+    const orderData: categorieDto = await ctx.req.json();
+    const result = await this.categorieUseCase.create(orderData);
+    if (typeof result === "string") {
+      return ctx.json("Error");
+    }
+    return ctx.json(result);
+  }
+
+  async update(ctx: Context) {
+    const orderData: categorieDto = await ctx.req.json();
+    const result = await this.categorieUseCase.update(orderData);
+    if (typeof result === "string") {
+      return ctx.json({ message: result });
+    }
+    return ctx.json(result);
+  }
+
+  async delete(ctx: Context) {
+    const name = await ctx.req.json();
+    const result = await this.categorieUseCase.delete(name);
+    if (typeof result === "string") {
+      return ctx.json({ message: result });
+    }
+    return ctx.json({ message: result });
+  }
+
+  async getAll(ctx: Context) {
+    const result = await this.categorieUseCase.getAll();
+    if (typeof result === "string") {
+      return ctx.json({ message: result });
+    }
+    return ctx.json({ message: result });
+  }
 }
-
-
-=======
-//import type { Context } from "hono";
-//
-//export class CategorieController implements CategorieUseCase {
-//    
-//} drf
-//
->>>>>>> panier
--- src/Infrastructure/http/routes/market/cart.ts
+++ src/Infrastructure/http/routes/market/cart.ts
@@ -1,28 +1,7 @@
-import { Hono } from "hono";
-import { CartController } from "../../controllers/cartController.js";
-import { CartUseCase } from "../../../../Application/usecases/cartUseCase.js";
-import { CartRepoImpl } from "../../../repositories/cartRepoImpl.js";
-
-const cartRepositories = new CartRepoImpl();
-const cartUseCase = new CartUseCase(cartRepositories);
-const cartController = new CartController(cartUseCase);
-
-export const cartRoutes = new Hono();
-cartRoutes.post('/', async (c) =>{
-  const res = await cartController.getByUserID(c);
-  return c.json({ response: res });  
-})
-cartRoutes.post('/update', async (c) => {
-    return cartController.deleteCart(c);
-});
-cartRoutes.delete('/delete', async (c) => {
-    return cartController.deleteCart(c);
-});
-
 import { Hono } from 'hono';
 import { CartController } from '../../controllers/cartController.js';
 import { CartUseCase } from '../../../../Application/usecases/cartUseCase.js';
-import { CartRepoImpl } from '../../../repositories/cardRepoImpl.js';
+import { CartRepoImpl } from '../../../repositories/cartRepoImpl.js';
 
 const cartRepository = new CartRepoImpl();
 export const cartUseCase = new CartUseCase(cartRepository);
