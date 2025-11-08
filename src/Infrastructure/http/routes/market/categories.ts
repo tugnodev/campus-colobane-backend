b6e5967b3b6bbdd1a058698ed4f42dced608237b
ebeydi <ebeydif@gmail.com>
2025-11-08 12:17:59 +0000

    Update categories.ts

--- src/Infrastructure/http/routes/market/categories.ts
+++ src/Infrastructure/http/routes/market/categories.ts
@@ -1,31 +1,29 @@
 import { Hono } from "hono";
 import { CategorieController } from "../../controllers/categorieController.js";
 import { CategorieUseCase } from "../../../../Application/usecases/categorieUseCase.js";
-import { CategorieRepoImpl } from "../../../../Infrastructure/repositories/categorieRepoImp.js";
-import { WebSocketHandler } from "../../../../Infrastructure/websocket/websocketService.js";
+import { CategorieRepoImpl } from "../../../repositories/categorieRepoImpl.js";
 
 const categorieRepository = new CategorieRepoImpl();
-const notificationService = new WebSocketHandler();
-const categorieUseCase = new CategorieUseCase(categorieRepository, notificationService);
+const categorieUseCase = new CategorieUseCase(categorieRepository);
 const categorieController = new CategorieController(categorieUseCase);
 
 export const categorieRoutes = new Hono();
-categorieRoutes.post('/create', async (c) => {
-    return categorieController.create(c);
+categorieRoutes.post("/create", async (c) => {
+  return categorieController.create(c);
 });
-chatRoutes.get('/', async (c) => {
-    return c.json({ message: "Route Categorie" });
+categorieRoutes.get("/", async (c) => {
+  return c.json({ message: "Route Categorie" });
 });
-categorieRoutes.patch('/update', async (c) => {
-    return categorieController.update(c);
+categorieRoutes.patch("/update", async (c) => {
+  return categorieController.update(c);
 });
-categorieRoutes.delete('/delete', async (c) => {
-    return categorieController.delete(c);
+categorieRoutes.delete("/delete", async (c) => {
+  return categorieController.delete(c);
 });
 // Register specific routes BEFORE dynamic ones to avoid collisions
-categorieRoutes.get('/all', async (c) => {
-    return c.json({ message: "Route Categorie" });
+categorieRoutes.get("/all", async (c) => {
+  return c.json({ message: "Route Categorie" });
 });
-categoRoutes.get('/:name', async (c) => {
-    return messageController.getByUserId(c);
+categorieRoutes.get("/:name", async (c) => {
+  return categorieController.getAll(c);
 });
