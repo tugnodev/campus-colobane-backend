069d609fb73b7a00d8e07774bd1546594a0bcfa2
ebeydi <ebeydif@gmail.com>
2025-11-09 18:48:47 +0000

    Update index.ts

--- src/Infrastructure/http/routes/market/commandes.ts
+++ src/Infrastructure/http/routes/market/commandes.ts
@@ -7,22 +7,20 @@ const OrderRepo = new OrderRepoImpl();
 const CommandeUseCase = new OrderUseCase(OrderRepo);
 const CommandeController = new OrderController(CommandeUseCase);
 
-const Commanrouter = new Hono();
-Commanrouter.post("/commandes", async(c) =>{
+export const CommandeRoutes = new Hono();
+CommandeRoutes.post("/commandes", async (c) => {
   await CommandeController.create(c);
 });
-Commanrouter.put("/commandes/:id", async(c) =>{
+CommandeRoutes.put("/commandes/:id", async (c) => {
   await CommandeController.update(c);
 });
-Commanrouter.delete("/commandes/:id", async(c) =>{
+CommandeRoutes.delete("/commandes/:id", async (c) => {
   await CommandeController.delete(c);
 });
 
-Commanrouter.get("/commandes/:SellerId", async(c) =>{
+CommandeRoutes.get("/commandes/:SellerId", async (c) => {
   await CommandeController.getBySellerId(c);
 });
-Commanrouter.get("/commandes/:BuyerId", async(c) =>{
+CommandeRoutes.get("/commandes/:BuyerId", async (c) => {
   await CommandeController.getByBuyerId(c);
 });
-
-export default Commanrouter;
